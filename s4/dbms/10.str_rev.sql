9824ae1fb72cd2c3def128ecc00a7d8a89b85c02
joeljtomy <122166595+joeljtomy@users.noreply.github.com>
2024-07-12 09:39:49 +0530

    cleanup

--- s4/dbms/10.str_rev.sql
+++ s4/dbms/10.str_rev.sql
@@ -4,11 +4,11 @@ set serveroutput on;
 DECLARE
     str VARCHAR(20) := '&str';
     rev_str VARCHAR(20) := '';
-    i INT := LENGTH(str);
+    l INT := LENGTH(str);
 BEGIN
-    WHILE i > 0 LOOP
-        rev_str := (rev_str || SUBSTR(str, i, 1));
-        i := i - 1;
+    WHILE l > 0 LOOP
+        rev_str := (rev_str || SUBSTR(str, l, 1));
+        l := l - 1;
     END LOOP;
     dbms_output.Put_line('Reversed String: ' || rev_str);
 END;
